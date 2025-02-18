126be939ae8eb44dc3d4d8bb2dfc564c0157d526
juanjose9009 <81190386+juanjose9009@users.noreply.github.com>
2025-02-18 23:42:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.get("/partidos", async (req, res) => {
 });
 
 // Iniciar el servidor
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
